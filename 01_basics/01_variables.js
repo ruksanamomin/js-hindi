c0436d73485fea0b7c859489695ec5ddfca7b330
Ruksana shaik <100222994+ruksanamomin@users.noreply.github.com>
2024-06-11 15:33:39 +0000

    Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId=123456;
+let accountName="ruksana@gmail.com";
+var accountPassword = '123@ruk';
+accountCity = "Hyderabad";
+let accountState;
+
+// accountId = 2 // not allowed
+console.log(accountId);
+
+accountName = 'roshan@google.com'
+accountPassword='456'
+accountCity='Jaipur' 
+
+console.table([accountId,accountName,accountPassword,accountCity]);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
