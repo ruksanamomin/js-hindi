f8a1e8a7834f30af33ec480f004d1c86eac96335
Ruksana shaik <100222994+ruksanamomin@users.noreply.github.com>
2024-06-11 15:33:59 +0000

    Datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; //treat all Js code as new version or new Js code
+
+// alert(3+3); // We are using nodejs not browser. JS engine is there in browser so above syntac works fine in browser.But not in node js.
+
+//code should be readable and future proof
+ console.log(3
+    +3) ;// code readability should be high
+    /*tc39.es ECMS Script wrote the standard os JS not JS https://tc39.es/ecma262/
+     AND MDN BY mozilla organization  https://developer.mozilla.org/en-US/docs/Web/JavaScript */
+
+
+   //   Primitive datatypes
+   let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// NON-PRIMITIVE DATATYPES
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
