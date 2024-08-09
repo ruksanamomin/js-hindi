e0d9514297a455ae6ad5f699497910002a06ec5b
Ruksana shaik <100222994+ruksanamomin@users.noreply.github.com>
2024-08-09 18:34:13 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -30,12 +30,29 @@ function sayMyName(){
 //to get the exact result value other than undefine we have to make some changes to function declaration as follows
 
 function addTwoNumbers(number1,number2){
-    let result=number1+number2;
-    return result; //nothing should not be written after return and the code doesnt execute.
+    // let result =number1+number2;
+    // return result;//6
+    
+    return number1+number2;//6
     
 }
-const result = addTwoNumbers(5,4);
-console.log("Result:", result);
+//addTwoNumbers(1,5);//6
+ const result =addTwoNumbers(1,5);
+//console.log(result); //6
 
+//user enter uername annd we need to send a message logged in
+//function loginUserMessage(username){
+    function loginUserMessage(username = "sam"){ //if u never want to go to if statement we use the deafult value as sam
+    //if(username === undefined){ //line 45 and 46 are same 
+    if(!username){
+        console.log("Please enter username");
+        return
+
+    }
+    return`${username} just logged in`;
+}
+console.log(loginUserMessage());//sam just logged in
+console.log(loginUserMessage("hitesh")); //hitesh just logged in
+//NOTE: undefined and ""(empty string) are considered as false values.
 
 
