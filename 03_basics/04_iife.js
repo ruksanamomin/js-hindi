c7053ac3d1c9322880784a1106cc5ea1ce00d009
Ruksana shaik <100222994+ruksanamomin@users.noreply.github.com>
2024-08-10 14:46:49 +0000

    iife

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,29 @@
+//IIFE--- Immediately invoked function expression
+
+//We have two reason why we go for iife
+//1)we want the function to get executed immediately.
+//reason:: In db connection, we need the connection asap so we need the function to execute immediately.
+//2)when we do not want the function to get polluted with the global scope.
+//reason:: we use global scope variables in the block scope of functions , then wee do not want the 
+//function to get polluted with the global scope variables.
+
+//basic was of writing function
+// function chai(){
+//     console.log(`DB Connected`);
+    
+// }
+
+// chai(); //DB Connected
+
+//iife syntax
+//(function definition)(function call(argument));
+
+//named iife
+(function chai(){
+    console.log(`DB Connected`);
+}) ();
+ 
+//unnamed iife
+((name)=>{
+    console.log(`DB Connected two , ${name}`);
+})("ruksana")
