e66ca3b4860f80c867ee3e7f7d6320dc6e315a78
Ruksana shaik <100222994+ruksanamomin@users.noreply.github.com>
2024-08-10 11:45:59 +0000

    scope

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,68 @@
+//SCOPE
+/*scope ---> {} 
+two types of scope.1)block scope ...everything that is written in this is scope{} ex: may be function or if else.
+2)global scope----?Anything mentioned out of {} is known as blocklevel..
+Note::1)Anything mentioned in global scope can be used in its scope  as well as block scope.
+2)Anything mentioned in block scope will be limited to block scope only.
+
+ex::::case1
+
+
+*/
+//case1
+let a=10;
+const b=20;
+var c=30;
+//console.log(a); // 10
+//console.log(b);//20
+//console.log(c);//30
+
+if (true) {
+    let d=10; 
+const e=20;
+var c=300;
+}
+
+//console.log(d); // undefined coz block scope only we can use
+//console.log(e);//undefined coz block scope only we can use
+//console.log(c);//300 coz of var...var dont follow the rule of block scope elements should work within scoplevel(block scope) and works in global scope also
+//that is why var is not used to declare variables.
+
+//Note::::scope would be different when we run in browser console and code runtime environemnt using node.
+
+//NESTED SCOPE in NESTED FUNCTIONS AND IF ELSE
+
+function one(){
+    let username ="ruksana";
+    function two(){
+        let website = "youtube";
+        //console.log(username); //ruksana //child can use parents elements
+        
+    }
+ //console.log(website); //error sayine website is not defined. as parent cannot use child's elements or values...
+ 
+    two();
+}
+
+one();
+
+//if else to explain nested loop
+if(true){
+    const username ="abdul";
+    //console.log(username);
+    if(username === "abdul"){
+        const website= ' chai aur code'
+        //console.log( username + website);
+        
+    }
+
+    //console.log(website); // error website is not defined coz child cannot be used by parent
+    
+    
+}
+
+//console.log(username); //error : username is not defined coz it is used out of its scope
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
