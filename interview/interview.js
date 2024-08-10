a38a0d5788ab59cb040510f076f94afa1c66c5cd
Ruksana shaik <100222994+ruksanamomin@users.noreply.github.com>
2024-08-10 17:45:58 +0000

    interview

---
+++ interview/interview.js
@@ -0,0 +1,9 @@
+//JS EXECUTION CONTEXT
+//whenever u make a file, js executes and runs the file...js runs the file   in  2phases.
+
+//whenever we make a file and gives it to Js..then everytime happens a GLOBAL EC.
+//this GEC is kept inside a variable named THIS.
+//when we run GEC in browser  "this" value would be a window object.but it differes in node ,bun,dino.
+//JS is a single threaded lamguage
+
+//we have Global EC,Function EC. Eval EC(in Mangoose)
