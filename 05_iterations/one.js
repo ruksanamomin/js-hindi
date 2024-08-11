23e4d928762c56bbc44b0e89a62c3f5f6ed34725
Ruksana shaik <100222994+ruksanamomin@users.noreply.github.com>
2024-08-11 17:49:54 +0000

    for loop

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -2,7 +2,13 @@
 
 //syntax
 
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
     
+// }
+
+for(index=0;index<10;index++){
+        const element = index;
+        console.log(element);
+        
 }
