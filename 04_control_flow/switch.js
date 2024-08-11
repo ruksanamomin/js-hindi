d2526a5a4ed6a4b4e5afd7475d4dcc884a2191e0
Ruksana shaik <100222994+ruksanamomin@users.noreply.github.com>
2024-08-11 12:47:15 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,31 @@
+//one value multiple checks or checkpoints there we use switch....In Redux
+//General Syntax
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+//USE CASE STUDY
+const month = 2;
+switch(month){
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("february");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+        default:
+        console.log("default case");
+        break;
+        }
+//************************************** */
