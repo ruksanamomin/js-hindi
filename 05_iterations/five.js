86902b74489432a809ac58ac614974516e7e1572
Ruksana shaik <100222994+ruksanamomin@users.noreply.github.com>
2024-08-12 12:05:07 +0000

    FOR EACH

---
+++ 05_iterations/five.js
@@ -0,0 +1,20 @@
+//FOR EACH LOOP
+
+const coding = ["js", "ruby", "java", "python", "cpp"]
+//In array as length,amp,fill..for each is also an inbuilt method
+
+//for each is higher order function
+
+//coding.forEach(callbackfunction)
+
+//WAY1
+coding.forEach(function(index){
+//console.log(index);
+
+});
+
+//Way2 callback function ()=>{}
+coding.forEach((num)=>{
+console.log(num);
+
+})
