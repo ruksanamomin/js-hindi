4ee7d73c66d490b780733dcbfa2f46117fdb66a6
Ruksana shaik <100222994+ruksanamomin@users.noreply.github.com>
2024-08-13 03:39:33 +0000

    map

---
+++ 05_iterations/seven.js
@@ -0,0 +1,12 @@
+//MAP
+
+const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] 
+
+//using foreach and add 10 to each number
+
+myNumers.forEach((item)=> {
+    let result=item+10;
+  console.log(result);
+  }
+);
+
